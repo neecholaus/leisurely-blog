7430be57ed282c81618ddf37ba0beadd1cc5316e
neecholaus <thisisnickadam@gmail.com>
2023-05-04 10:30:59 -0700

    feat(templating-scaffolding)_

--- routes.go
+++ routes.go
@@ -1,11 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func getLanding(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("landing page"))
+	html, _ := template.ParseFiles("./html/core-template.html", "./html/landing.html")
+	_ = html.Execute(w, nil)
 }
 
 func getAbout(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("about page"))
+	html, _ := template.ParseFiles("./html/core-template.html", "./html/about.html")
+	_ = html.Execute(w, nil)
 }
